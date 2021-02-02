a5a88c3900ae226c09982f3d73163d55524c51d3
Aleksey Parfyonov <kantengri3@gmail.com>
2021-02-02 21:18:03 +0500

    move_base_mod before compile

--- trajectory_planner/move_base_mod/include/move_base/move_base_mod.h
+++ trajectory_planner/move_base_mod/include/move_base/move_base_mod.h
@@ -34,8 +34,8 @@
 *
 * Author: Eitan Marder-Eppstein
 *********************************************************************/
-#ifndef NAV_MOVE_BASE_ACTION_H_
-#define NAV_MOVE_BASE_ACTION_H_
+#ifndef NAV_MOVE_BASE_MOD_ACTION_H_
+#define NAV_MOVE_BASE_MOD_ACTION_H_
 
 #include <vector>
 #include <string>
@@ -43,11 +43,11 @@
 #include <ros/ros.h>
 
 #include <actionlib/server/simple_action_server.h>
-#include <move_base_msgs/MoveBaseAction.h>
+#include <move_base/TrajectoryAction.h>
 
 #include <nav_core/base_local_planner.h>
-#include <nav_core/base_global_planner.h>
-#include <nav_core/recovery_behavior.h>
+// #include <nav_core/base_global_planner.h>
+// #include <nav_core/recovery_behavior.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <costmap_2d/costmap_2d.h>
@@ -56,12 +56,12 @@
 #include <pluginlib/class_loader.hpp>
 #include <std_srvs/Empty.h>
 
-#include <dynamic_reconfigure/server.h>
-#include "move_base/MoveBaseConfig.h"
+// #include <dynamic_reconfigure/server.h>
+// #include "move_base/MoveBaseConfig.h"
 
-namespace move_base {
+namespace move_base_mod {
   //typedefs to help us out with the action server so that we don't hace to type so much
-  // typedef actionlib::SimpleActionServer<move_base_msgs::MoveBaseAction> MoveBaseActionServer;
+  typedef actionlib::SimpleActionServer<move_base_mod::TrajectoryControllerAction> MoveBaseActionServer;
 
   // enum MoveBaseState {
   //   PLANNING,
@@ -108,7 +108,7 @@ namespace move_base {
        * @param resp The service response
        * @return True if the service call succeeds, false otherwise
        */
-      bool clearCostmapsService(std_srvs::Empty::Request &req, std_srvs::Empty::Response &resp);
+      // bool clearCostmapsService(std_srvs::Empty::Request &req, std_srvs::Empty::Response &resp);
 
       /**
        * @brief  A service call that can be made when the action is inactive that will return a plan
@@ -116,7 +116,7 @@ namespace move_base {
        * @param  resp The plan request
        * @return True if planning succeeded, false otherwise
        */
-      bool planService(nav_msgs::GetPlan::Request &req, nav_msgs::GetPlan::Response &resp);
+      // bool planService(nav_msgs::GetPlan::Request &req, nav_msgs::GetPlan::Response &resp);
 
       /**
        * @brief  Make a new global plan
@@ -124,7 +124,7 @@ namespace move_base {
        * @param  plan Will be filled in with the plan made by the planner
        * @return  True if planning succeeds, false otherwise
        */
-      bool makePlan(const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
+      // bool makePlan(const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
 
       /**
        * @brief  Load the recovery behaviors for the navigation stack from the parameter server
@@ -157,22 +157,15 @@ namespace move_base {
 
       void goalCB(const geometry_msgs::PoseStamped::ConstPtr& goal);
 
-      void planThread();
-
       void executeCb(const move_base_msgs::MoveBaseGoalConstPtr& move_base_goal);
 
       bool isQuaternionValid(const geometry_msgs::Quaternion& q);
 
       bool getRobotPose(geometry_msgs::PoseStamped& global_pose, costmap_2d::Costmap2DROS* costmap);
 
-      double distance(const geometry_msgs::PoseStamped& p1, const geometry_msgs::PoseStamped& p2);
+      // double distance(const geometry_msgs::PoseStamped& p1, const geometry_msgs::PoseStamped& p2);
 
       geometry_msgs::PoseStamped goalToGlobalFrame(const geometry_msgs::PoseStamped& goal_pose_msg);
-
-      /**
-       * @brief This is used to wake the planner at periodic intervals.
-       */
-      void wakePlanner(const ros::TimerEvent& event);
 
       tf2_ros::Buffer& tf_;
 
@@ -181,56 +174,56 @@ namespace move_base {
       boost::shared_ptr<nav_core::BaseLocalPlanner> tc_;
       costmap_2d::Costmap2DROS* planner_costmap_ros_, *controller_costmap_ros_;
 
-      boost::shared_ptr<nav_core::BaseGlobalPlanner> planner_;
+      // boost::shared_ptr<nav_core::BaseGlobalPlanner> planner_;
       std::string robot_base_frame_, global_frame_;
 
-      std::vector<boost::shared_ptr<nav_core::RecoveryBehavior> > recovery_behaviors_;
-      std::vector<std::string> recovery_behavior_names_;
-      unsigned int recovery_index_;
+      // std::vector<boost::shared_ptr<nav_core::RecoveryBehavior> > recovery_behaviors_;
+      // std::vector<std::string> recovery_behavior_names_;
+      // unsigned int recovery_index_;
 
       geometry_msgs::PoseStamped global_pose_;
-      double planner_frequency_, controller_frequency_, inscribed_radius_, circumscribed_radius_;
-      double planner_patience_, controller_patience_;
-      int32_t max_planning_retries_;
-      uint32_t planning_retries_;
-      double conservative_reset_dist_, clearing_radius_;
+      // double planner_frequency_, controller_frequency_, inscribed_radius_, circumscribed_radius_;
+      // double planner_patience_, controller_patience_;
+      // int32_t max_planning_retries_;
+      // uint32_t planning_retries_;
+      // double conservative_reset_dist_, clearing_radius_;
       ros::Publisher current_goal_pub_, vel_pub_, action_goal_pub_, recovery_status_pub_;
       ros::Subscriber goal_sub_;
-      ros::ServiceServer make_plan_srv_, clear_costmaps_srv_;
-      bool shutdown_costmaps_, clearing_rotation_allowed_, recovery_behavior_enabled_;
-      bool make_plan_clear_costmap_, make_plan_add_unreachable_goal_;
-      double oscillation_timeout_, oscillation_distance_;
+      // ros::ServiceServer make_plan_srv_, clear_costmaps_srv_;
+      // bool shutdown_costmaps_, clearing_rotation_allowed_, recovery_behavior_enabled_;
+      // bool make_plan_clear_costmap_, make_plan_add_unreachable_goal_;
+      // double oscillation_timeout_, oscillation_distance_;
 
-      MoveBaseState state_;
-      RecoveryTrigger recovery_trigger_;
+      // MoveBaseState state_;
+      // RecoveryTrigger recovery_trigger_;
 
-      ros::Time last_valid_plan_, last_valid_control_, last_oscillation_reset_;
-      geometry_msgs::PoseStamped oscillation_pose_;
-      pluginlib::ClassLoader<nav_core::BaseGlobalPlanner> bgp_loader_;
+      // ros::Time last_valid_plan_, last_valid_control_, last_oscillation_reset_;
+      // geometry_msgs::PoseStamped oscillation_pose_;
+      // pluginlib::ClassLoader<nav_core::BaseGlobalPlanner> bgp_loader_;
       pluginlib::ClassLoader<nav_core::BaseLocalPlanner> blp_loader_;
-      pluginlib::ClassLoader<nav_core::RecoveryBehavior> recovery_loader_;
+      // pluginlib::ClassLoader<nav_core::RecoveryBehavior> recovery_loader_;
 
       //set up plan triple buffer
-      std::vector<geometry_msgs::PoseStamped>* planner_plan_;
-      std::vector<geometry_msgs::PoseStamped>* latest_plan_;
+      // std::vector<geometry_msgs::PoseStamped>* planner_plan_;
+      // std::vector<geometry_msgs::PoseStamped>* latest_plan_;
       std::vector<geometry_msgs::PoseStamped>* controller_plan_;
 
       //set up the planner's thread
-      bool runPlanner_;
-      boost::recursive_mutex planner_mutex_;
-      boost::condition_variable_any planner_cond_;
-      geometry_msgs::PoseStamped planner_goal_;
-      boost::thread* planner_thread_;
+      // bool runPlanner_;
+      // boost::recursive_mutex planner_mutex_;
+      // boost::condition_variable_any planner_cond_;
+      // geometry_msgs::PoseStamped planner_goal_;
+      // boost::thread* planner_thread_;
 
 
-      boost::recursive_mutex configuration_mutex_;
-      dynamic_reconfigure::Server<move_base::MoveBaseConfig> *dsrv_;
+      // boost::recursive_mutex configuration_mutex_;
+      // dynamic_reconfigure::Server<move_base::MoveBaseConfig> *dsrv_;
       
       void reconfigureCB(move_base::MoveBaseConfig &config, uint32_t level);
 
-      move_base::MoveBaseConfig last_config_;
-      move_base::MoveBaseConfig default_config_;
-      bool setup_, p_freq_change_, c_freq_change_;
+      // move_base::MoveBaseConfig last_config_;
+      // move_base::MoveBaseConfig default_config_;
+      // bool setup_, p_freq_change_, c_freq_change_;
       bool new_global_plan_;
   };
 };
--- trajectory_planner/move_base_mod/src/move_base_node.cpp
+++ trajectory_planner/move_base_mod/src/move_base_node.cpp
@@ -27,15 +27,15 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <move_base/move_base.h>
+#include <move_base/move_base_mod.h>
 #include <tf2_ros/transform_listener.h>
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "move_base_node");
+  ros::init(argc, argv, "move_base_mod_node");
   tf2_ros::Buffer buffer(ros::Duration(10));
   tf2_ros::TransformListener tf(buffer);
 
-  move_base::MoveBase move_base( buffer );
+  move_base_mod::MoveBase move_base( buffer );
 
   //ros::MultiThreadedSpinner s;
   ros::spin();
