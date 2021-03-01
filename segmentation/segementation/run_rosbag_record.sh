fc1b6a7e86e1cddfb3502bef30b17924e6c2ffc0
Aleksey Parfyonov <kantengri3@gmail.com>
2021-03-01 15:44:32 +0500

    rosbag rec segm

--- segmentation/segementation/run_rosbag_record.sh
+++ segmentation/segementation/run_rosbag_record.sh
@@ -8,20 +8,11 @@
     . "/opt/ros/$ROS_DISTRO/setup.bash"
 set -ex
     rosbag record -o $1 \
-        rosout \
-        rosout_agg \
         tf \
         tf_static \
-        /mobile_base/commands/velocity \
-        /camera/depth/camera_info \
-        /camera/depth/image_raw \
-        /camera/depth/points \
-        /camera/parameter_descriptions \
-        /camera/parameter_updates \
+        /segmentation_node/segmentation_vis \
+        /pcl_proc/points2 \
         /clock \
-        /camera/rgb/image_raw/compressed \
-        /velodyne_points \
-        /odom \
         __name:=my_bag
     rosnode kill /my_bag
     exit 0
