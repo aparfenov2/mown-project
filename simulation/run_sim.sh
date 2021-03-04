9042a30e236099d2a304e7b1be47ab54b095c1e0
Aleksey Parfyonov <kantengri3@gmail.com>
2021-03-04 16:40:34 +0500

    fix sim ws

--- simulation/run_sim.sh
+++ simulation/run_sim.sh
@@ -10,7 +10,7 @@
     exit 0
 }
 
-bash _run_in_docker.sh --script $0 --image ros-mower \
+bash _run_in_docker.sh --script $0 --name gazebo_sim \
     -v $(readlink -f simulator_ws/src/tb_gazebo):/cdir/simulator_ws/src/tb_gazebo \
     -v $(readlink -f simulator_ws/src/tb_gazebo_description):/cdir/simulator_ws/src/tb_gazebo_description \
     -v $(readlink -f simulator_ws/src/tb_gazebo_msgs):/cdir/simulator_ws/src/tb_gazebo_msgs \
