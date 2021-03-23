d6348b2dfb5de00d403073e2c3b99d5538d7eebc
Aleksey Parfyonov <kantengri3@gmail.com>
2021-03-23 13:58:05 +0500

    downgrade velodyne sim ver

--- simulation/run_sim.sh
+++ simulation/run_sim.sh
@@ -10,8 +10,17 @@
     exit 0
 }
 
-bash _run_in_docker.sh --script $0 --name gazebo_sim \
-    -v $(readlink -f simulator_ws/src/tb_gazebo):/cdir/simulator_ws/src/tb_gazebo \
-    -v $(readlink -f simulator_ws/src/tb_gazebo_description):/cdir/simulator_ws/src/tb_gazebo_description \
-    -v $(readlink -f simulator_ws/src/tb_gazebo_msgs):/cdir/simulator_ws/src/tb_gazebo_msgs \
-    -v $(readlink -f simulator_ws/src/tb_gazebo_plugins):/cdir/simulator_ws/src/tb_gazebo_plugins
+pushd $PWD
+cd simulator_ws/src
+[ -d "velodyne_simulator" ] || {
+    git clone https://bitbucket.org/DataspeedInc/velodyne_simulator.git
+}
+cd velodyne_simulator
+git checkout 1.0.9
+popd
+
+bash _run_in_docker.sh --script $0 --name gazebo_sim 
+    # -v $(readlink -f simulator_ws/src/tb_gazebo):/cdir/simulator_ws/src/tb_gazebo \
+    # -v $(readlink -f simulator_ws/src/tb_gazebo_description):/cdir/simulator_ws/src/tb_gazebo_description \
+    # -v $(readlink -f simulator_ws/src/tb_gazebo_msgs):/cdir/simulator_ws/src/tb_gazebo_msgs \
+    # -v $(readlink -f simulator_ws/src/tb_gazebo_plugins):/cdir/simulator_ws/src/tb_gazebo_plugins
