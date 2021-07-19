2c2656e5784acba83825187cfe900fc76b0b1a62
Aleksey Parfyonov <kantengri3@gmail.com>
2021-07-19 12:53:02 +0500

    using frontier explr; global costmap as bound map

---
+++ common/my_utils_common/src/map_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.srv import GetMap, GetMapResponse
+from nav_msgs.msg import OccupancyGrid
+
+class MapServer:
+    def __init__(self):
+        self.map = None
+
+    def grid_callback(self, data):
+        self.map = data
+
+    def handle_get_map(self, req):
+        return GetMapResponse(self.map)
+
+    def main(self):
+        rospy.init_node('map_server')
+        rospy.Service('static_map', GetMap, self.handle_get_map)
+        rospy.Subscriber("/map", OccupancyGrid, self.grid_callback)
+        print("Ready to add two ints.")
+        rospy.spin()
+
+if __name__ == "__main__":
+    MapServer().main()
