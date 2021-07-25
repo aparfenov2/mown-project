fa0b06f7ded60c8663cdb362254c2afca517e4bc
Aleksey Parfyonov <kantengri3@gmail.com>
2021-07-25 14:44:54 +0500

    polygon costmap moved out of move_base

--- common/my_utils_common/src/map_server.py
+++ common/my_utils_common/src/map_server.py
@@ -12,13 +12,15 @@ class MapServer:
         self.map = data
 
     def handle_get_map(self, req):
+        assert self.map is not None
+        rospy.loginfo('handle_get_map: map returned')
         return GetMapResponse(self.map)
 
     def main(self):
         rospy.init_node('map_server')
         rospy.Service('static_map', GetMap, self.handle_get_map)
         rospy.Subscriber("/map", OccupancyGrid, self.grid_callback)
-        print("Ready to add two ints.")
+        rospy.loginfo("Ready to add two ints.")
         rospy.spin()
 
 if __name__ == "__main__":
