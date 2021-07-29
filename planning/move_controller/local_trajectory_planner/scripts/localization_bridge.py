b30e24e2eed62ab63b6ab5ac5e47180d9e3808f5
Konstantin <kos16@mail.ru>
2021-07-29 17:43:48 +0300

    feat (move_controller): load topic name from params

--- planning/move_controller/local_trajectory_planner/scripts/localization_bridge.py
+++ planning/move_controller/local_trajectory_planner/scripts/localization_bridge.py
@@ -16,7 +16,8 @@ class OdometryToLocalizationNode(AbstractNode):
         self.localization_publisher = rospy.Publisher('/localization', Localization, queue_size=10)
 
         # rospy.Subscriber('/laser_odom_to_init', Odometry, self.__odometry_callback) 
-        rospy.Subscriber('/ground_truth/state', Odometry, self.__odometry_callback)
+        odometry_topic = rospy.get_param('/planning/odometry', '/ground_truth/state')
+        rospy.Subscriber(odometry_topic, Odometry, self.__odometry_callback)
 
     def __odometry_callback(self, message: Odometry):
 
