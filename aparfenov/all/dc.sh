ef1701ea0754387ce18704695ed5045a335ce6be
Aleksey.Parfenov <Aleksey.Parfenov@waveaccess.global>
2023-10-28 11:21:33 +0500

    better docker-compose.yaml without dependencies

--- aparfenov/all/dc.sh
+++ aparfenov/all/dc.sh
@@ -1,18 +1,46 @@
-python gen_docker_compose.py
 
 [ -z "$1" ] && {
     echo specify up [service_name] or down [something]
     exit 1
 }
 
+VOLUMES=()
+for f in $(find ws/src -type l); do
+    VOLUMES+=("$(readlink -f $f):/cdir/$f")
+done
+
+[ -e "data" ] && {
+    VOLUMES+=("$(readlink -f data):/cdir/data")
+}
+DCG="_dc-generated.yaml"
+echo "version: \"3.9\"" > $DCG
+echo "x-common-mounts: &common-mounts" >> $DCG
+echo "    - ./.ros:/root/.ros" >> $DCG
+echo "    - /dev:/dev" >> $DCG
+echo "    - ./:/cdir" >> $DCG
+echo "    - /home/.gazebo/models:/root/.gazebo/models" >> $DCG
+
+for f in "${VOLUMES[@]}"; do
+    echo "    - $f" >> $DCG
+done
+
+cat docker-compose.yaml >> $DCG
+
+DOCKER_COMPOSE="docker-compose -f $DCG"
+
 [ "$1" == "up" ] && {
     shift
-    docker-compose up --no-recreate $@
+    ${DOCKER_COMPOSE} up --no-recreate $@
     exit $?
 }
 [ "$1" == "down" ] && {
     shift
-    docker-compose down --remove-orphans $@
+    ${DOCKER_COMPOSE} down --remove-orphans $@
     exit $?
 }
-docker-compose $@
+[ "$1" == "run" ] && {
+    shift
+    ${DOCKER_COMPOSE} run --rm $@
+    exit $?
+}
+${DOCKER_COMPOSE} $@
