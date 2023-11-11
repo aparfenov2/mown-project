bb693cc147bc3e3a108002a3ecc21e450f224542
Aleksey.Parfenov <Aleksey.Parfenov@waveaccess.global>
2023-11-11 22:04:42 +0500

    makefile; fix planning

--- aparfenov/all/dc.sh
+++ aparfenov/all/dc.sh
@@ -36,25 +36,6 @@ while [[ "$#" -gt 0 ]]; do
     }
 done
 
-VOLUMES=()
-for f in $(find ws/src -type l); do
-    if ! readlink -f $f > /dev/null; then
-        echo "failed to map $f"
-    fi
-    VOLUMES+=("$(readlink -f $f):/cdir/$f")
-done
-
-[ -e "data" ] && {
-    VOLUMES+=("$(readlink -f data):/cdir/data")
-}
-DCG="docker-compose.yaml"
-echo "version: \"3.9\"" > $DCG
-echo "x-common-mounts: &common-mounts" >> $DCG
-for f in "${VOLUMES[@]}"; do
-    echo "    - $f" >> $DCG
-done
-
-cat docker-compose.main.yaml >> $DCG
-xhost +
+call gen_docker_compose.sh
 set -ex
 ROSARGS="${ROSARGS[@]}" docker compose ${DCARGS[@]}
