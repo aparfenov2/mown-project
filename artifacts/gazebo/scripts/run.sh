2120edad90384f5f20a686d643e3a33404aa172a
UsenkoKonstantinVL <kos16@mail.ru>
2023-11-12 22:33:12 +0300

    feat: Add backend to modules

--- artifacts/gazebo/scripts/run.sh
+++ artifacts/gazebo/scripts/run.sh
@@ -7,7 +7,7 @@ rosdep update
 WORLD="empty"
 ROBOT="turtlebot"
 USE_GAZEBO_SIM_TIME="True"
-GUI="True"
+GUI="False"
 
 
 roslaunch simulator_launch gazebo.launch world:=$WORLD robot:=$ROBOT use_sim_time_in_gazebo:=$USE_GAZEBO_SIM_TIME gui:=$GUI
--- artifacts/old/all_jetson_ml/try_build.sh
+++
@@ -1,1 +0,0 @@
-../all/try_build.sh
